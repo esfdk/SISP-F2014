aa6283c4682dee57770de273919f7613a582e364
jcgr <jcgr@itu.dk>
2014-03-13 14:25:10 +0100

    Started Eval

--- ConnectFour/src/HelperClass.java
+++ ConnectFour/src/HelperClass.java
@@ -5,7 +5,52 @@ public class HelperClass
 {
 	public static int Eval(int[][] board, int player)
 	{
+		int result = 0;
+		
+		result += EvalHorizontal(board, player);
+		result += EvalVertical(board, player);
+		result += EvalDiagonalLeft(board, player);
+		result += EvalDiagonalRight(board, player);
+		
 		// TODO: Implement this
+		return result;
+	}
+	
+	private static int EvalHorizontal(int[][] board, int playerID)
+	{
+		for (int cols = 0; cols < board.length; cols++)
+		{
+			for (int rows = 0; rows < board[0].length; rows++)
+			{
+				if (board[cols][rows] == playerID)
+				{
+					
+				}
+				else if (board[cols][rows] == nextPlayer(playerID))
+				{
+					
+				}
+			}
+		}
+		
+		return 0;
+	}
+	
+	private static int EvalVertical(int[][] board, int player)
+	{
+		
+		return 0;
+	}
+	
+	private static int EvalDiagonalLeft(int[][] board, int player)
+	{
+		
+		return 0;
+	}
+	
+	private static int EvalDiagonalRight(int[][] board, int player)
+	{
+		
 		return 0;
 	}
 
